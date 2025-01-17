95eed4d704c44c19849383822d4f11d8d9ec0282
Hamou <ryad.hamou@student.vinci.be>
2025-01-17 16:32:13 +0100

    feature : second checker (special character)

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,6 +1,14 @@
 function isValidPassword(password) {
+
+    //Check if the password is less than 8 characters
     if (password.length < 8) {
         return false;
+    }
+
+    //Check if the password contains a special character
+    const specialCharacters = /[!_-|@#]/;
+    if(!specialCharacters.test(password)) {
+        return false
     }
     return true;
 }
